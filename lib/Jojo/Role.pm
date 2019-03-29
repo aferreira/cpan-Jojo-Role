244837370c03e062ab9d12da4af9e40bd44fbcff
Adriano Ferreira <a.r.ferreira@gmail.com>
2019-03-29 10:52:00 -0700

    Simplify Jojo::Role using Jojo::Role::Tiny

--- lib/Jojo/Role.pm
+++ lib/Jojo/Role.pm
@@ -10,45 +10,34 @@ use feature      ();
 use experimental ();
 
 BEGIN {
-  require Role::Tiny;
-  Role::Tiny->VERSION('2.000006');
-  our @ISA = qw(Role::Tiny);
+  require Jojo::Role::Tiny;
+  Jojo::Role::Tiny->VERSION('2.000006');
+  our @ISA = qw(Jojo::Role::Tiny);
 }
 
 use Sub::Inject 0.3.0 ();
 
-# Aliasing of Role::Tiny symbols
+# Aliasing of Jojo::Role::Tiny symbols
 BEGIN {
-  *INFO           = \%Role::Tiny::INFO;
-  *APPLIED_TO     = \%Role::Tiny::APPLIED_TO;
-  *COMPOSED       = \%Role::Tiny::COMPOSED;
-  *COMPOSITE_INFO = \%Role::Tiny::COMPOSITE_INFO;
-  *ON_ROLE_CREATE = \@Role::Tiny::ON_ROLE_CREATE;
-
-  *_getstash = \&Role::Tiny::_getstash;
+  *INFO = \%Jojo::Role::Tiny::INFO;
 }
 
 our %INFO;
-our %APPLIED_TO;
-our %COMPOSED;
-our %COMPOSITE_INFO;
-our @ON_ROLE_CREATE;
 
 our %EXPORT_TAGS;
 our %EXPORT_GEN;
 
-
 # Jojo::Role->apply_roles_to_package('Some::Package', qw(Some::Role +Other::Role));
 sub apply_roles_to_package {
   my ($self, $target) = (shift, shift);
-  return $self->Role::Tiny::apply_roles_to_package($target,
+  return $self->Jojo::Role::Tiny::apply_roles_to_package($target,
     map { /^\+(.+)$/ ? "${target}::Role::$1" : $_ } @_);
 }
 
 # Jojo::Role->create_class_with_roles('Some::Base', qw(Some::Role1 +Role2));
 sub create_class_with_roles {
   my ($self, $target) = (shift, shift);
-  return $self->Role::Tiny::create_class_with_roles($target,
+  return $self->Jojo::Role::Tiny::create_class_with_roles($target,
     map { /^\+(.+)$/ ? "${target}::Role::$1" : $_ } @_);
 }
 
@@ -73,31 +62,6 @@ sub import {
 }
 
 sub role_provider { $_[0] }
-
-sub make_role {
-  my ($me, $target) = @_;
-  return if $me->is_role($target);    # already exported into this package
-  $INFO{$target}{is_role} = 1;
-
-  # get symbol table reference
-  my $stash = _getstash($target);
-
-  # grab all *non-constant* (stash slot is not a scalarref) subs present
-  # in the symbol table and store their refaddrs (no need to forcibly
-  # inflate constant subs into real subs) with a map to the coderefs in
-  # case of copying or re-use
-  my @not_methods
-    = map +(ref $_ eq 'CODE' ? $_ : ref $_ ? () : *$_{CODE} || ()),
-    values %$stash;
-  @{$INFO{$target}{not_methods} = {}}{@not_methods} = @not_methods;
-
-  # a role does itself
-  $APPLIED_TO{$target} = {$target => undef};
-  foreach my $hook (@ON_ROLE_CREATE) {
-    $hook->($target);
-  }
-  return;
-}
 
 BEGIN {
   %EXPORT_TAGS = (    #
